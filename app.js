339b361401a0025d89d2b9b73c692bb9fc028bfa
Cole Nelson <ctnelson1997@gmail.com>
2024-11-20 08:45:32 -0600

    hw10

---
+++ app.js
@@ -0,0 +1,9 @@
+const BOOKS = getBooks();
+
+const booksDivNode = document.getElementById("book-list")
+
+// TODO Construct and add a book component for each book to the book-list! 
+
+function createBookComponent(bookData) {
+    // TODO Create and return a node for the given bookData!
+}
